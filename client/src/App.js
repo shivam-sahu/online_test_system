7e98dd00b6c6d6763150f87f7ae52985cc0a470b
shivam <shivam14sahu@gmail.com>
2019-08-17 00:05:24 +0530

    basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React,{Component} from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -16,20 +15,7 @@ class App extends Component {
 
     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Hello!
     </div>
   );}
 }
